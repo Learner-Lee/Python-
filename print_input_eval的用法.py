5d461fb1f455ff95ec112005b11245e39289d485
Learner-Lee <89835879+Learner-Lee@users.noreply.github.com>
2022-10-04 11:38:00 +0800

    Add files via upload

---
+++ print_input_eval的用法.py
@@ -0,0 +1,12 @@
+print ("hello")
+print ("hello",end="***")
+print ("*******\nHow are you!\n************")
+hello="你好"
+eval("hello")#hello是变量
+a=eval(input("请输入一个数"))
+a=a+23
+print(a)
+eval("1.2+3.4")
+a=input("请输入一个数")
+a=a*3
+print(a)
