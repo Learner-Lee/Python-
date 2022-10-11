06fb18518c4d9e6a2165aa572d184dd82f03af04
Learner-Lee <89835879+Learner-Lee@users.noreply.github.com>
2022-10-11 20:47:35 +0800

    Add files via upload

---
+++ math函数.py
@@ -0,0 +1,11 @@
+# math库的使用，不支持复数计算
+# math.pi:圆周率
+# math.e :自然对数
+# math.inf :正无穷大
+# -math.inf :负无穷大
+
+import math
+r=5
+h=10
+v=math.pi*r**2*h/3
+print(v)
---
+++ 进制与复数.py
@@ -0,0 +1,10 @@
+x=456
+bin(x)#二进制
+oct(x)#八进制
+hex(x)#十六进制
+
+complex(1,2)#复数类型
+n=2+6j
+print(n)#（2+6j)原括号不能缺损
+print(n.real)#实部2.0
+print(n.imag)#虚部6.0
